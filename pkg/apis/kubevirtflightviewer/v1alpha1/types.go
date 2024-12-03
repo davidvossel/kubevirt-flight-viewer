029b90c02321494fad0e89dbf33f09f999b256dd
David Vossel <davidvossel@gmail.com>
2024-12-03 15:38:32 -0500

    consistent printer columns
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- pkg/apis/kubevirtflightviewer/v1alpha1/types.go
+++ pkg/apis/kubevirtflightviewer/v1alpha1/types.go
@@ -62,8 +62,9 @@ type InFlightClusterOperationList struct {
 // +kubebuilder:printcolumn:name="Operation_Type",type="string",JSONPath=".status.operationType",description="Operation Type"
 // +kubebuilder:printcolumn:name="Resource_Kind",type="string",JSONPath=".metadata.ownerReferences[0].kind",description="Resource Kind"
 // +kubebuilder:printcolumn:name="Resource_Name",type="string",JSONPath=".metadata.ownerReferences[0].name",description="Resource Name"
-// +kubebuilder:printcolumn:name="Progressing",type="string",JSONPath=".status.conditions[?(@.type==\"Progressing\")].status",description="Progressing"
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description="Age"
+// +kubebuilder:printcolumn:name="Message",type="string",JSONPath=".metadata.ownerReferences[0].message",description="Message"
+
 type InFlightOperation struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
